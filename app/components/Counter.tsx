256c4316c0086bde489c2cb67ef92c3ac2d4bb12
romeoscript <romeobourne211@gmail.com>
2023-08-26 21:11:03 +0100

    feat:done

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Counter = () => {
   return (
-    <div className='bg-[#F16232] flex justify-around items-center p-[4rem] my-[4rem]'>
-        <div className='text-center p-[1rem]'>
-            <h2 className='font-bold text-4xl my-[1rem]'>+1M</h2>
-            <p>Data Points Analyzed</p>
+    <section className=" my-[4rem]">
+      <h1 className="font-bold text-center text-5xl my-[1rem] text-black">
+        Why Choose us?
+      </h1>
+      <div className="bg-[#F16232] p-[4rem] flex justify-around items-center ">
+        <div className="text-center p-[1rem]">
+          <h2 className="font-bold text-4xl my-[1rem]">+1M</h2>
+          <p>Data Points Analyzed</p>
         </div>
-        <div className='text-center p-[1rem]'>
-            <h2 className='font-bold text-4xl my-[1rem]'>2023</h2>
-            <p>Founded</p>
+        <div className="text-center p-[1rem]">
+          <h2 className="font-bold text-4xl my-[1rem]">2023</h2>
+          <p>Founded</p>
         </div>
-        <div className='text-center p-[1rem]'>
-            <h1 className='font-bold text-4xl my-[1rem]'>500+</h1>
-            <p>Verified Customers</p>
+        <div className="text-center p-[1rem]">
+          <h1 className="font-bold text-4xl my-[1rem]">500+</h1>
+          <p>Verified Customers</p>
         </div>
-    </div>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
-export default Counter
+export default Counter;
