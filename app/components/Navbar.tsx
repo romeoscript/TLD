2300d88b272938c207fb1b2283c03c3c77320a68
romeoscript <romeobourne211@gmail.com>
2023-08-31 06:48:31 +0100

    feat:minor-change

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+import Link from "next/link";
 
 const navbar = () => {
   return (
     <div className="flex justify-between h-[80px] bg-[#F16232] items-center px-[2rem] top-0 fixed w-full z-30">
       <h2 className="text-3xl">TLD</h2>
       <ul className="flex items-center gap-[2rem]">
-        <li>Home</li>
-        <li>Service</li>
-        <li>Blog</li>
-        <li>About Us</li>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/">
+          <li>Service</li>
+        </Link>
+        <Link href="/projects">
+          <li>Projects</li>
+        </Link>
+        <Link href="/about">
+          <li>About Us</li>
+        </Link>
       </ul>
-      <button className="bg-white rounded-md text-[#F16232] px-[2rem] py-[0.5rem]">Get Started</button>
+      <button className="bg-white rounded-md text-[#F16232] px-[2rem] py-[0.5rem]">
+        Get Started
+      </button>
     </div>
   );
 };
