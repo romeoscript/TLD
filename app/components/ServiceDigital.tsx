08617094ab6947bdd8828ee27b7dc09103f54327
romeoscript <romeobourne211@gmail.com>
2023-09-03 14:44:12 +0100

    feat:digital

--- app/components/ServiceDigital.tsx
+++ app/components/ServiceDigital.tsx
@@ -3,13 +3,13 @@ import brandimg from "../images/brandimg.png";
 
 const ServiceDigital = () => {
   return (
-    <div className="flex items-center justify-between p-[3rem]">
+    <div className="md:flex items-center justify-between p-[3rem]  block">
       <div className="basis-[50%]">
         <img src={brandimg.src} alt="" />
       </div>
-      <div className="text-black basis-[50%]">
-        <h2 className="text-4xl leading-[1.2] capitalize w-3/5">full service digital marketing agency</h2>
-        <p className="my-[1rem] w-3/5 leading-[1.6]">
+      <div className="text-black md-mt-0 mt-[1.5rem] basis-[50%]">
+        <h2 className="text-4xl leading-[1.2] capitalize md:w-3/5">full service digital marketing agency</h2>
+        <p className="my-[1rem] md:w-3/5 leading-[1.6]">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
           mollitia at, quam dicta, expedita aperiam voluptas placeat facilis
           iure, officia ducimus omnis animi ut porro dignissimos fugit. Rem,
