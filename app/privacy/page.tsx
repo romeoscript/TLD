6a71aadbd201130e11f0f414456d7c73c5b1019f
Romanus Chukwuemeka <romeobourne211@gmail.com>
2024-12-06 15:44:11 +0100

    Update page.tsx

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -4,10 +4,10 @@ import React from "react";
 const PrivacyPolicy = () => {
   return (
     <div className="max-w-4xl mx-auto p-6 space-y-6 text-black">
-      <h1 className="text-3xl font-bold text-center">Privacy Policy for Timelines Digitals</h1>
+      <h1 className="text-3xl font-bold text-center">Privacy Policy for Timeline Digitals Agency</h1>
 
       <p className="text-lg">
-        At Timeline Digitals, accessible at timelinedigitals.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Timelines Digitals and how we use it. If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us through email at{" "}
+        At Timeline Digitals Agency, accessible at timelinedigitals.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Timelines Digitals and how we use it. If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us through email at{" "}
         <a href="mailto:support@timelinedigitals.com" className="text-blue-600">support@timelinedigitals.com</a>.
       </p>
 
